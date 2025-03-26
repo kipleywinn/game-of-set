992bdb3f1c93eee0e4641e0aa4c4a8f603fab2db
Carissa Nicole Winn <carissanicole12@gmail.com>
2025-03-25 21:58:18 -0400

    added initial timer script

--- js/app.js
+++ js/app.js
@@ -607,7 +607,7 @@ function startTimeCounter() {
 }
 
 function checkTime(i) {
-  if (i < 10) { i = `0${i}`};  // add zero in front of numbers < 10
+  if (i < 10) { i = `0${i}`}  // add zero in front of numbers < 10
   return i;
 }
 
