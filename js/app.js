5884819cd0f2685b18ce6f21eac7410efa30544f
Carissa Nicole Winn <carissanicole12@gmail.com>
2025-03-25 21:57:07 -0400

    added initial timer script

--- js/app.js
+++ js/app.js
@@ -591,8 +591,27 @@ function newGame() {
 }
 
 
+/////// NEW TIMER FUNCTION
+let startTime = Math.floor(Date.now() / 1000); //Get the starting time (right now) in seconds
+localStorage.setItem("startTime", startTime); // Store it if I want to restart the timer on the next page
 
+function startTimeCounter() {
+  let now = Math.floor(Date.now() / 1000); // get the time now
+  let diff = now - startTime; // diff in seconds between now and start
+  let m = Math.floor(diff / 60); // get minutes value (quotient of diff)
+  let s = Math.floor(diff % 60); // get seconds value (remainder of diff)
+  m = checkTime(m); // add a leading zero if it's single digit
+  s = checkTime(s); // add a leading zero if it's single digit
+  document.getElementById("gameTimer").innerHTML = `${m}:${s}`; // update the element where the timer will appear
+  let t = setTimeout(startTimeCounter, 1000); // set a timeout to update the timer
+}
 
+function checkTime(i) {
+  if (i < 10) { i = `0${i}`};  // add zero in front of numbers < 10
+  return i;
+}
+
+startTimeCounter();
 
 
 
